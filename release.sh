2eb23e8b5aacf85fbd30e6a495b6d6237f2dbe43
JavaCommons Technologies <javacommons@gmail.com>
2025-04-21 02:36:32 +0900

    .

--- release.sh
+++ release.sh
@@ -3,13 +3,12 @@ set -uvx
 set -e
 cwd=`pwd`
 ts=`date "+%Y.%m%d.%H%M"`
-version="${ts//.0/.}"
+version=$(echo $ts | sed "s/[.]0*/./g")
 comment=$1
 
 echo $version
 
 pubspec "$version"
-#dart pub get
 
 cat << EOS >> CHANGELOG.md
 
@@ -24,11 +23,10 @@ dos2unix CHANGELOG.md
 ./do-analyze.sh
 ./do-test.sh
 
-#exit 0
-
-tag="$version"
+cd $cwd
+tag="sys-$version"
 git add .
-git commit -m"$comment"
+git commit -m"sys: $comment"
 git tag -a "$tag" -m"$tag"
 git push origin "$tag"
 git push origin HEAD:main
