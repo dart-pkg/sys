e1c005dd4f3241b05b12818e9a1dfe17d67e90c7
JavaCommons Technologies <javacommons@gmail.com>
2025-04-30 18:54:14 +0900

    sys: Added: pathExpand()

--- lib/sys.dart
+++ lib/sys.dart
@@ -1,58 +1,60 @@
 import 'dart:core';
-import 'dart:convert' as convert__;
-import 'dart:io' as io__;
+import 'dart:convert' as dart_conver;
+import 'dart:io' as dart_io;
 import 'dart:typed_data';
-import 'package:path/path.dart' as path__;
-import 'package:http/http.dart' as http__;
-// import 'package:std/command_runner.dart' as std__;
-// import 'package:std/misc.dart' as std__;
-import 'package:std/std.dart' as std__;
-import 'package:archive/archive.dart' as archive__;
+import 'package:path/path.dart' as path_path;
+import 'package:http/http.dart' as http_http;
+import 'package:std/std.dart' as std_std;
+import 'package:archive/archive.dart' as archive_archive;
 
 bool get isInDebugMode {
-  return std__.isInDebugMode;
+  return std_std.isInDebugMode;
 }
 
 String? getenv(String name) {
-  return std__.getenv(name);
+  return std_std.getenv(name);
+}
+
+String pathExpand(String path) {
+  return std_std.pathExpand(path);
 }
 
 void setCwd(String $path) {
-  io__.Directory.current = pathFullName($path);
+  dart_io.Directory.current = pathFullName($path);
 }
 
 String getCwd() {
-  return pathFullName(io__.Directory.current.absolute.path);
+  return pathFullName(dart_io.Directory.current.absolute.path);
 }
 
 String pathFullName(String $path) {
-  return path__.normalize(path__.absolute($path)).replaceAll(r'\', '/');
+  return path_path.normalize(path_path.absolute($path)).replaceAll(r'\', '/');
 }
 
 String pathDirectoryName(String $path) {
-  return pathFullName(path__.dirname($path));
+  return pathFullName(path_path.dirname($path));
 }
 
 String pathFileName(String $path) {
-  return path__.basename($path);
+  return path_path.basename($path);
 }
 
 String pathBaseName(String $path) {
-  return path__.basenameWithoutExtension($path);
+  return path_path.basenameWithoutExtension($path);
 }
 
 String pathExtension(String $path) {
-  return path__.extension($path);
+  return path_path.extension($path);
 }
 
 List<String> _getFilesFromDirRecursive(String $path) {
   List<String> result = [];
-  io__.Directory dir = io__.Directory($path);
-  List<io__.FileSystemEntity> entities = dir.listSync().toList();
+  dart_io.Directory dir = dart_io.Directory($path);
+  List<dart_io.FileSystemEntity> entities = dir.listSync().toList();
   for (var entity in entities) {
-    if (entity is io__.File) {
+    if (entity is dart_io.File) {
       result.add(pathFullName(entity.path));
-    } else if (entity is io__.Directory) {
+    } else if (entity is dart_io.Directory) {
       result.addAll(_getFilesFromDirRecursive(pathFullName(entity.path)));
     }
   }
@@ -67,9 +69,9 @@ List<String> pathFiles(String $path, [bool? $recursive]) {
         $path,
       ).map(($x) => $x.replaceAll(r'\', r'/')).toList();
     }
-    final $dir = io__.Directory(path__.join($path));
-    final List<io__.FileSystemEntity> $entities = $dir.listSync().toList();
-    final Iterable<io__.File> $files = $entities.whereType<io__.File>();
+    final $dir = dart_io.Directory(path_path.join($path));
+    final List<dart_io.FileSystemEntity> $entities = $dir.listSync().toList();
+    final Iterable<dart_io.File> $files = $entities.whereType<dart_io.File>();
     List<String> result = [];
     $files.toList().forEach((x) {
       result.add(pathFullName(x.path));
@@ -82,10 +84,10 @@ List<String> pathFiles(String $path, [bool? $recursive]) {
 
 List<String> pathDirectories(String $path) {
   try {
-    final $dir = io__.Directory(path__.join($path));
-    final List<io__.FileSystemEntity> $entities = $dir.listSync().toList();
-    final Iterable<io__.Directory> $dirs =
-        $entities.whereType<io__.Directory>();
+    final $dir = dart_io.Directory(path_path.join($path));
+    final List<dart_io.FileSystemEntity> $entities = $dir.listSync().toList();
+    final Iterable<dart_io.Directory> $dirs =
+        $entities.whereType<dart_io.Directory>();
     List<String> result = [];
     $dirs.toList().forEach((x) {
       result.add(pathFullName(x.path));
@@ -97,41 +99,41 @@ List<String> pathDirectories(String $path) {
 }
 
 Uint8List readFileBytes(String path) {
-  return std__.readFileBytes(path);
+  return std_std.readFileBytes(path);
 }
 
 String readFileString(String path) {
-  return std__.readFileString(path);
+  return std_std.readFileString(path);
 }
 
 List<String> readFileLines(String path) {
-  return std__.readFileLines(path);
+  return std_std.readFileLines(path);
 }
 
 void writeFileBytes(String path, Uint8List data) {
-  std__.writeFileBytes(path, data);
+  std_std.writeFileBytes(path, data);
 }
 
 void writeFileString(String path, String data) {
-  std__.writeFileString(path, data);
+  std_std.writeFileString(path, data);
 }
 
 List<String> textToLines(String s) {
-  return std__.textToLines(s);
+  return std_std.textToLines(s);
 }
 
 bool fileExists(String path) {
-  return std__.fileExists(path);
+  return std_std.fileExists(path);
 }
 
 bool directoryExists(String path) {
-  return std__.directoryExists(path);
+  return std_std.directoryExists(path);
 }
 
 Future<String?> httpGetBodyAsync(String $urlString) async {
   try {
     var url = Uri.parse($urlString);
-    var response = await http__.get(url);
+    var response = await http_http.get(url);
     if (response.statusCode != 200) {
       return null;
     }
@@ -146,7 +148,7 @@ Future<dynamic> runAsync(
   bool returnCode = false,
   bool useBash = false,
 }) async {
-  final $run = std__.CommandRunner(useUnixShell: useBash);
+  final $run = std_std.CommandRunner(useUnixShell: useBash);
   return $run.run(command, returnCode: returnCode);
 }
 
@@ -164,7 +166,7 @@ Future<dynamic> runAsync$(
     ..addAll(rest);
   // String $executable = $list[0];
   // List<String> $arguments = $list.sublist(1).toList();
-  final $run = std__.CommandRunner(useUnixShell: useBash);
+  final $run = std_std.CommandRunner(useUnixShell: useBash);
   return $run.run$(
     // $executable,
     // arguments: $arguments,
@@ -176,26 +178,26 @@ Future<dynamic> runAsync$(
 
 String lastChars(String s, int len) {
   // return s.substring(s.length - len);
-  return std__.lastChars(s, len);
+  return std_std.lastChars(s, len);
 }
 
 String timeBasedVersionString() {
-  return std__.timeBasedVersionString();
+  return std_std.timeBasedVersionString();
 }
 
 void unzipToDirectory(String zipPath, String destDir) {
   zipPath = pathFullName(zipPath);
   destDir = pathFullName(destDir);
-  final bytes = io__.File(zipPath).readAsBytesSync();
-  final archive = archive__.ZipDecoder().decodeBytes(bytes);
+  final bytes = dart_io.File(zipPath).readAsBytesSync();
+  final archive = archive_archive.ZipDecoder().decodeBytes(bytes);
   for (final entry in archive) {
     if (entry.isFile) {
       var fileBytes = entry.readBytes();
       fileBytes = fileBytes!;
-      if (std__.isText(fileBytes)) {
-        String text = convert__.utf8.decode(fileBytes);
-        text = std__.adjustTextNewlines(text);
-        fileBytes = convert__.utf8.encode(text);
+      if (std_std.isText(fileBytes)) {
+        String text = dart_conver.utf8.decode(fileBytes);
+        text = std_std.adjustTextNewlines(text);
+        fileBytes = dart_conver.utf8.encode(text);
       }
       writeFileBytes('$destDir/${entry.name}', fileBytes);
     }
